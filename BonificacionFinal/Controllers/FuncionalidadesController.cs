61f42db5697eb5952f1f07cadb7c8c50bb92e119
Santiagog048 <gallo9290@gmail.com>
2024-06-13 20:53:29 -0500

    Agregar archivos de proyecto.

---
+++ BonificacionFinal/Controllers/FuncionalidadesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using BonificacionFinal;
+
+namespace BonificacionFinal.Controllers
+{
+    public class FuncionalidadesController : Controller
+    {
+        private BonificacionFinalEntities db = new BonificacionFinalEntities();
+
+        // GET: Funcionalidades/Buscar
+        public ActionResult Buscar()
+        {
+            return View();
+        }
+
+        // POST: Funcionalidades/Buscar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Buscar(string codigoCurso)
+        {
+            if (string.IsNullOrEmpty(codigoCurso))
+            {
+                ModelState.AddModelError("", "Debe ingresar un código de curso.");
+                return View();
+            }
+
+            Curso curso = db.Curso.FirstOrDefault(c => c.CodigoCurso == codigoCurso);
+            if (curso == null)
+            {
+                ModelState.AddModelError("", "Curso no encontrado.");
+                return View();
+            }
+
+            return View("DetallesCurso", curso);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BonificacionFinal/Controllers/MatriculasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BonificacionFinal;
+
+namespace BonificacionFinal.Controllers
+{
+    public class MatriculasController : Controller
+    {
+        private BonificacionFinalEntities db = new BonificacionFinalEntities();
+
+        // GET: Matriculas
+        public ActionResult Index()
+        {
+            var matricula = db.Matricula.Include(m => m.Curso).Include(m => m.Estudiante);
+            return View(matricula.ToList());
+        }
+
+        // GET: Matriculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Create
+        public ActionResult Create()
+        {
+            ViewBag.CursoId = new SelectList(db.Curso, "CursoId", "CodigoCurso");
+            ViewBag.EstudianteId = new SelectList(db.Estudiante, "EstudianteId", "Identificacion");
+            return View();
+        }
+
+        // POST: Matriculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MatriculaId,EstudianteId,CursoId")] Matricula matricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Matricula.Add(matricula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CursoId = new SelectList(db.Curso, "CursoId", "CodigoCurso", matricula.CursoId);
+            ViewBag.EstudianteId = new SelectList(db.Estudiante, "EstudianteId", "Identificacion", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CursoId = new SelectList(db.Curso, "CursoId", "CodigoCurso", matricula.CursoId);
+            ViewBag.EstudianteId = new SelectList(db.Estudiante, "EstudianteId", "Identificacion", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MatriculaId,EstudianteId,CursoId")] Matricula matricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(matricula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CursoId = new SelectList(db.Curso, "CursoId", "CodigoCurso", matricula.CursoId);
+            ViewBag.EstudianteId = new SelectList(db.Estudiante, "EstudianteId", "Identificacion", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Matricula matricula = db.Matricula.Find(id);
+            db.Matricula.Remove(matricula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
